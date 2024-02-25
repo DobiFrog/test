c2996172b7a60bcc3669037f43764fabf88b9ee1
DobiFrog <kseniakyz@gmail.com>
2024-02-25 18:54:42 +0400

    проверка ветки

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("i = " + i);
 
             System.out.println("пипиркапппп");
-            System.out.println("пипиркапппп");
+            System.out.println("пипиркаппапрарпп");
         }
     }
 }
