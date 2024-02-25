d80c0f938f42e060c5557d1d3c48ef2af38e49bf
DobiFrog <kseniakyz@gmail.com>
2024-02-25 15:43:04 +0400

    версия1

---
+++ src/PlayerServiceJSON.java
@@ -0,0 +1,28 @@
+import java.util.Collection;
+
+public class PlayerServiceJSON implements PlayerService {
+    @Override
+    public Player getPlayerById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Player> getPlayers() {
+        return null;
+    }
+
+    @Override
+    public int createPlayer(String nickname) {
+        return 0;
+    }
+
+    @Override
+    public Player deletePlayer(int id) {
+        return null;
+    }
+
+    @Override
+    public int addPoints(int playerId, int points) {
+        return 0;
+    }
+}
